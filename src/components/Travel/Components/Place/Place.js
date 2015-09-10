f71b91667d4a51e4409ebc7cb78e6d0d87a7be30
keske <hello@andreykeske.com>
2015-09-10 15:01:29 +0200

    Place done

--- src/components/Travel/Components/Place/Place.js
+++ src/components/Travel/Components/Place/Place.js
@@ -10,18 +10,21 @@ export default class Place extends React.Component {
     params: React.PropTypes.object,
   }
 
-  contextTypes: {
-    router: React.PropTypes.func
-  }
-
   componentDidMount() {
     const { year, month, city } = this.props.params;
 
+    // Path to photos
     const img = `./images/${year}/${month}/${city}`;
+
+    // Template url
     const url = `./places/${year}/${month}/${city}/index.html`;
 
+    // Get html data from file
     $.get(url).done((data) => {
-      this.setState({data: this.insertDataToTemplate(data, img, city)});
+      this.setState({
+        // Replace photos urls and city name
+        data: this.replaceDataInTemplate(data, img, city),
+      });
     }.bind(this));
   }
 
@@ -45,7 +48,21 @@ export default class Place extends React.Component {
     data: null,
   }
 
-  insertDataToTemplate(data, img, city) {
-    return data.replace(/{{url}}/g, img).replace(/{{city}}/g, city);
+  /*
+   * Replace data in template
+   *
+   * Template has some code like {{url}} which
+   * we have to replace to real photo url
+   *
+   * @param {String} html code
+   * @param {String} path to image
+   * @param {String} city name
+   *
+   * @return {String} html code
+   */
+  replaceDataInTemplate(data, img, city) {
+    return data
+      .replace(/{{url}}/g, img)
+      .replace(/{{city}}/g, city);
   }
 }
