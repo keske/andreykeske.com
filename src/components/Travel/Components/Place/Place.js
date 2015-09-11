300a7bdec17a609a1d24aae038040d359bd03dc4
keske <hello@andreykeske.com>
2015-09-11 21:58:48 +0200

    Prague done

--- src/components/Travel/Components/Place/Place.js
+++ src/components/Travel/Components/Place/Place.js
@@ -7,13 +7,15 @@ import styles from './Place.styles.js';
 import {
   VeniceInMay2015,
   FlorenceInMay2015,
-  RomeInMay2015
+  RomeInMay2015,
+  PragueInJanuary2015
 } from './places/index.js';
 
 const data = {
   'VeniceInMay2015': VeniceInMay2015,
   'FlorenceInMay2015': FlorenceInMay2015,
   'RomeInMay2015': RomeInMay2015,
+  'PragueInJanuary2015': PragueInJanuary2015,
 };
 
 export default class Place extends Component {
