2b363b2dafdbd37cd58f7ca09613a3e1ed3c4c93
keske <hello@andreykeske.com>
2015-09-22 19:49:57 +0300

    *

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import { Router, Route } from 'react-router';
 import { Link } from 'react-router';
 
 // Actions
