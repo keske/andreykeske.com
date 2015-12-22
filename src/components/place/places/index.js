843635d7519c31732731e87d6c9106491fcc7531
keske <hello@andreykeske.com>
2015-12-22 18:18:42 +0300

    Tallinn fixes

--- src/components/place/places/index.js
+++ src/components/place/places/index.js
@@ -2,7 +2,7 @@
 export { default as BarcelonaInSeptember2015 } from './2015/September/Barcelona/index.js';
 
 // August 2015
-export { default as TallinInAugust2015 } from './2015/August/Tallin/index.js';
+export { default as TallinnInAugust2015 } from './2015/August/Tallinn/index.js';
 
 // May 2015
 export { default as VeniceInMay2015 } from './2015/May/Venice/index.js';
@@ -14,7 +14,7 @@ export { default as PragueInJanuary2015 } from './2015/January/Prague/index.js';
 export { default as MunichInJanuary2015 } from './2015/January/Munich/index.js';
 
 // March 2014
-export { default as TallinInMarch2014 } from './2014/March/Tallin/index.js';
+export { default as TallinnInMarch2014 } from './2014/March/Tallinn/index.js';
 
 // September 2014
 export { default as BerlinInSeptember2014 } from './2014/September/Berlin/index.js';
