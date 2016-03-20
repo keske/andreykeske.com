fd88b1aebf61751d4ebddeacd8f2a9f1b3080fd5
keske <hello@andreykeske.com>
2016-03-20 18:18:11 +0200

    Add Chapters I/II components

---
+++ app/components/Helpers/Travel/Chapters/I/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Component styles
+import { styles } from './styles.scss';
+
+const I = () =>
+  <div className={styles}>
+    <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+      Chapter I
+    </div>
+  </div>;
+
+export default I;
