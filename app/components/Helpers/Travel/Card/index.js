ce0d66c143104ca260237857e46341bbaa6479ed
keske <hello@andreykeske.com>
2016-03-21 18:56:22 +0300

    Travel vertical grid styles

--- app/components/Helpers/Travel/Card/index.js
+++ app/components/Helpers/Travel/Card/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import R from 'ramda';
 
 // Components
 import Large from './Large';
 import Medium from './Medium';
 
+// Component styles
+import { styles } from './styles.scss';
+
 const Card = place =>
-  place.viewMode === 'large'
-    ? <Large {...place} />
-    : <Medium {...place} />;
+  <span className={styles}>
+    {
+      R.equals(place.viewMode, 'large')
+        ? <Large {...place} />
+        : <Medium {...place} />
+    }
+  </span>;
 
 export default Card;
