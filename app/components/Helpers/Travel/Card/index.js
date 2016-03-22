0a6e253e9c1a7bbb48e9e7f8342c718bb9a7a172
keske <hello@andreykeske.com>
2016-03-23 01:04:15 +0300

    Card Ramda refacoring part II

--- app/components/Helpers/Travel/Card/index.js
+++ app/components/Helpers/Travel/Card/index.js
@@ -8,38 +8,21 @@ import Medium from './Medium';
 // Component styles
 import { styles } from './styles.scss';
 
-const Card = place => {
-
-  const renderAsCard =
-    R.ifElse(
-      R.equals(R.__, 'large'),
-      () => <Large {...place} />,
-      () => <Medium {...place} />
-    );
-
-  const renderAsText = () =>
-    <span className="render-cities-as-text">
-      {
-        place.city
-      }
-    </span>;
-
-  const render =
-    R.ifElse(
-      R.equals(R.__, true),
-      () => renderAsCard(place.viewMode),
-      () => renderAsText()
-    );
-
-  return (
-
-    <span className={styles}>
-      {
-        render(place.cover)
-      }
-    </span>
-  );
-
-};
+const Card = place =>
+  <span className={styles}>
+    {
+      R.ifElse(
+        R.equals(R.__, true),
+        () => R.ifElse(
+                R.equals(R.__, 'large'),
+                () => <Large {...place} />,
+                () => <Medium {...place} />
+              )(place.viewMode),
+        () => <span className="render-cities-as-text">
+                { place.city }
+              </span>
+      )(place.cover)
+    }
+  </span>;
 
 export default Card;
