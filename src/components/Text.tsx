eb7531e6cc875ce8be223f71c67a8c92fdfb6a49
Andrey Keske <hello@andreykeske.com>
2020-10-20 17:37:11 -0400

    Test Broken CI

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // Libs
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet,
+
+  css } from 'aphrodite';
 
 type Props = {
   children: JSX.Element;
@@ -11,8 +13,21 @@ const styles = StyleSheet.create({
   root: { color: 'red' },
 });
 
+const x = 0;
+
 const Text: React.FC<Props> = ({ children }: Props): JSX.Element => (
   <p className={css(styles.root)}>{children}</p>
 )
 
 export default Text;
+
+
+
+
+
+
+
+
+
+
+
