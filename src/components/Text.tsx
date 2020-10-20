34c44d303e44bf0d29a45412af23648a02abdd05
Andrey Keske <hello@andreykeske.com>
2020-10-20 17:42:34 -0400

    CI

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 // Libs
-import { StyleSheet,
-
-  css } from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite';
 
 type Props = {
   children: JSX.Element;
@@ -13,21 +11,8 @@ const styles = StyleSheet.create({
   root: { color: 'red' },
 });
 
-const x = 0;
-
 const Text: React.FC<Props> = ({ children }: Props): JSX.Element => (
   <p className={css(styles.root)}>{children}</p>
-)
+);
 
 export default Text;
-
-
-
-
-
-
-
-
-
-
-
