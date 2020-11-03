1ed62631490d1a5bc1bf2586a096d33b39066b97
Andrey Keske <hello@andreykeske.com>
2020-11-03 17:30:46 -0500

    Fix: Emojis From Behind

--- src/Hacks/EmojiBack.tsx
+++ src/Hacks/EmojiBack.tsx
@@ -17,7 +17,7 @@ const EmojiBack: React.FC = (): JSX.Element => (
   <ProjectWrapper>
     <Col lg={{ offset: 2, span: 6 }}>
       <UploadcareImage src="https://ucarecdn.com/f73ebc4a-f1bf-434c-b5b6-f9a8383b82f6/" />
-      <Title>Emoji From the Back</Title>
+      <Title>Emojis from behind</Title>
       <ProjectDisciplines disciplines={['Graphic Design']} />
       <ProjectTopics topics={['Emoji']} />
       <ProjectYear>2018</ProjectYear>
