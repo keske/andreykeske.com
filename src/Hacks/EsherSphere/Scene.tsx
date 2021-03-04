f5ae73935f4fbac2ee8169ef766b381df80f26d3
Andrey Keske <hello@andreykeske.com>
2021-03-03 20:27:29 -0500

    WebCam removed

--- src/Hacks/EsherSphere/Scene.tsx
+++ src/Hacks/EsherSphere/Scene.tsx
@@ -1,23 +1,12 @@
 /* eslint react/jsx-props-no-spreading: 0 */
-/* eslint no-multi-assign: 0 */
-/* eslint @typescript-eslint/ban-ts-comment: 0 */
 
 import React from 'react';
 
 // Libs
-import Webcam from 'react-webcam';
-import { extend } from 'react-three-fiber';
 import { Box, CubeCamera } from '@react-three/drei';
-
-extend({ Webcam });
 
 const Sphere = ({ ...props }: any) => {
   const ref = React.useRef<THREE.Group>();
-
-  // useFrame(({ clock }) => {
-  //   ref.current!.position.y =
-  //     Math.sin(offset + clock.elapsedTime) * 5;
-  // });
 
   return (
     <CubeCamera {...props}>
@@ -46,22 +35,6 @@ const Scene: React.FC = () => {
         args={[5, 5, 5]}
         material-color="hotpink"
         position-y={1.5}
-      />
-
-      {/* @ts-ignore} */}
-      <webcam
-        // @ts-ignore
-        audio={false}
-        // @ts-ignore
-        height="113"
-        // @ts-ignore
-        id="webcam"
-        // @ts-ignore
-        mirrored
-        // @ts-ignore
-        screenshotFormat="image/jpeg"
-        // @ts-ignore
-        width="200"
       />
 
       <gridHelper args={[100, 10]} />
