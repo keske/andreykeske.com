324cfe36dc774aa1645251de79912cb96dfe87bc
Andrey <hello@andreykeske.com>
2021-09-17 18:51:06 -0400

    SubTitle

--- src/components/SubTitle.tsx
+++ src/components/SubTitle.tsx
@@ -1,44 +1,19 @@
-import * as React from 'react';
-
-// Libs
-import { StyleSheet, css } from 'aphrodite';
+import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   children: React.ReactNode;
   invert?: boolean;
 };
 
-const DESKTOP_FONT_SIZE = 1.7;
+const Root = styled.h3<Props>`
+  color: ${({ invert }) => (invert ? 'white' : 'black')};
+  font-size: 24px;
+  font-weight: bold;
+`;
 
-const MOBILE_FONT_SIZE = 1.1;
-
-const getTypographySettings = (
-  fontSize: number,
-): Record<string, string> => ({
-  fontSize: `${fontSize}rem`,
-});
-
-const styles = ({ invert }: Pick<Props, 'invert'>) => ({
-  root: {
-    '@media screen and (max-width: 414px)': {
-      ...getTypographySettings(MOBILE_FONT_SIZE),
-    },
-    ...getTypographySettings(DESKTOP_FONT_SIZE),
-    color: invert ? 'white' : 'black',
-    fontSize: '1.9rem',
-    fontWeight: 800,
-    letterSpacing: '0.09rem',
-    marginTop: 40,
-  } as const,
-});
-
-const SubTitle: React.FC<Props> = ({
-  children,
-  invert = false,
-}: Props): JSX.Element => {
-  const cx = StyleSheet.create(styles({ invert }));
-
-  return <h3 className={css(cx.root)}>{children}</h3>;
-};
+const SubTitle: React.FC<Props> = ({ children, ...rest }: Props) => (
+  <Root {...rest}>{children}</Root>
+);
 
 export default SubTitle;
