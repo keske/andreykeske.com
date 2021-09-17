dd949a2598fdccdb458110b5cec244483e1726f6
Andrey <hello@andreykeske.com>
2021-09-17 18:43:21 -0400

    Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,58 +1,20 @@
-import * as React from 'react';
-
-// Libs
-import { StyleSheet, css } from 'aphrodite';
-
-// Local
-import Anchor from './Anchor';
+import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
-  children: string;
+  children: React.ReactNode;
   invert?: boolean;
 };
 
-const DESKTOP_FONT_SIZE = 2.5;
+const Root = styled.h3<Props>`
+  color: ${({ invert }) => (invert ? 'white' : 'black')};
+  font-size: 36px;
+  font-weight: bold;
+  margin: 40px 0 20px;
+`;
 
-const MOBILE_FONT_SIZE = 1.7;
-
-const getTypographySettings = (
-  fontSize: number,
-): Record<string, string> => ({
-  fontSize: `${fontSize}rem`,
-});
-
-const styles = ({ invert }: Pick<Props, 'invert'>) => ({
-  root: {
-    '@media screen and (max-width: 414px)': {
-      ...getTypographySettings(MOBILE_FONT_SIZE),
-      marginBottom: 10,
-      marginTop: 20,
-    },
-    ...getTypographySettings(DESKTOP_FONT_SIZE),
-    color: invert ? 'white' : 'black',
-    fontWeight: 800,
-    letterSpacing: `-.025rem`,
-    marginBottom: 20,
-    marginTop: 40,
-  } as const,
-});
-
-const Title: React.FC<Props> = ({
-  children,
-  invert = false,
-}: Props): JSX.Element => {
-  const cx = StyleSheet.create(styles({ invert }));
-
-  const getLink = () => children.replace(' ', '-').toLowerCase();
-
-  return (
-    <>
-      <Anchor link={getLink()} />
-      <h2 className={css(cx.root)} id={getLink()}>
-        {children}
-      </h2>
-    </>
-  );
-};
+const Title: React.FC<Props> = ({ children, ...rest }: Props) => (
+  <Root {...rest}>{children}</Root>
+);
 
 export default Title;
