37d36eb57e75e36079b02251bb6b95d070fa84b3
Andrey <hello@andreykeske.com>
2021-09-20 00:00:56 -0400

    Coords

--- src/Hacks/WarpedMinecraft/Cube.tsx
+++ src/Hacks/WarpedMinecraft/Cube.tsx
@@ -45,6 +45,10 @@ const Cube: React.FC = () => {
 
   const x1y0z3 = React.useMemo(() => [0, -2, -4], []);
 
+  const x1y3z2 = React.useMemo(() => [0, 2, -2], []);
+
+  const x1y3z1 = React.useMemo(() => [0, 2, -1], []);
+
   const x1y1z3 = React.useMemo(() => [0, -1, -4], []);
 
   const x1y2z3 = React.useMemo(() => [0, 1, -4], []);
@@ -62,6 +66,10 @@ const Cube: React.FC = () => {
   );
 
   const x1y0z0 = React.useMemo(() => [0, -2, 0], []);
+
+  const x1y0z2 = React.useMemo(() => [0, -2, -2], []);
+
+  const x1y0z1 = React.useMemo(() => [0, -2, 1], []);
 
   const x1y1z0 = React.useMemo(() => [0, -1, 0], []);
 
@@ -117,6 +125,8 @@ const Cube: React.FC = () => {
 
   const x2y0z3 = React.useMemo(() => [2, -2, -4], []);
 
+  const x2y0z1 = React.useMemo(() => [2, -2, -1], []);
+
   const x2y1z3 = React.useMemo(() => [2, -1, -4], []);
 
   const x2y2z3 = React.useMemo(() => [2, 1, -4], []);
@@ -154,6 +164,8 @@ const Cube: React.FC = () => {
   );
 
   const x0y0z3 = React.useMemo(() => [-2, -2, -4], []);
+
+  const x0y3z1 = React.useMemo(() => [-2, 2, -1], []);
 
   const x0y1z3 = React.useMemo(() => [-2, -1, -4], []);
 
@@ -204,15 +216,15 @@ const Cube: React.FC = () => {
           getLeftEdgeOfBottomFace,
           [
             new THREE.Vector4(...x1y0z3, 0.1),
-            new THREE.Vector4(0, -2, -2, 0.1),
-            new THREE.Vector4(0, -2, -1, 0.1),
-            new THREE.Vector4(0, -2, 0, 0.1),
+            new THREE.Vector4(...x1y0z2, 0.1),
+            new THREE.Vector4(...x1y0z1, 0.1),
+            new THREE.Vector4(...x1y0z0, 0.1),
           ],
           [
-            new THREE.Vector4(2, -2, -4, 0.1),
-            new THREE.Vector4(2, -2, -2, 0.1),
-            new THREE.Vector4(2, -2, -1, 0.1),
-            new THREE.Vector4(2, -2, 0, 0.1),
+            new THREE.Vector4(...x2y0z3, 0.1),
+            new THREE.Vector4(...x2y0z2, 0.1),
+            new THREE.Vector4(...x2y0z1, 0.1),
+            new THREE.Vector4(...x2y0z0, 0.1),
           ],
         ],
         url: 'static/minecraft/dirt.jpg',
@@ -263,14 +275,14 @@ const Cube: React.FC = () => {
           ],
           [
             new THREE.Vector4(...x1y3z3, 0.1),
-            new THREE.Vector4(0, 2, -2, 0.1),
-            new THREE.Vector4(0, 2, -1, 0.1),
+            new THREE.Vector4(...x1y3z2, 0.1),
+            new THREE.Vector4(...x1y3z1, 0.1),
             new THREE.Vector4(...x1y3z0, 0.1),
           ],
           [
             new THREE.Vector4(...x0y3z3, 0.1),
             new THREE.Vector4(...x0y3z2, 0.1),
-            new THREE.Vector4(-2, 2, -1, 0.1),
+            new THREE.Vector4(...x0y3z1, 0.1),
             new THREE.Vector4(...x0y3z0, 0.1),
           ],
         ],
@@ -287,14 +299,28 @@ const Cube: React.FC = () => {
       getMiddleEdgeOfRightFace,
       getRightEdgeOfFrontFace,
       getRightEdgeOfRightFace,
+      x0y3z0,
+      x0y3z1,
+      x0y3z2,
+      x0y3z3,
+      x1y0z0,
+      x1y0z1,
+      x1y0z2,
+      x1y0z3,
+      x1y3z0,
+      x1y3z1,
+      x1y3z2,
+      x1y3z3,
+      x2y0z0,
+      x2y0z1,
+      x2y0z2,
+      x2y0z3,
       x2y3z0,
       x2y3z1,
       x2y3z2,
       x2y3z3,
     ],
   );
-
-  console.log(net);
 
   return (
     <group>
