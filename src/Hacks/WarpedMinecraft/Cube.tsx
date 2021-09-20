4068d88c48cc4c1554aeec5fcbd407d888b9d1bf
Andrey <hello@andreykeske.com>
2021-09-19 22:53:00 -0400

    Factoring out

--- src/Hacks/WarpedMinecraft/Cube.tsx
+++ src/Hacks/WarpedMinecraft/Cube.tsx
@@ -7,21 +7,101 @@ import * as THREE from 'three';
 import Face from './Cube.Face';
 
 const Cube: React.FC = () => {
-  const getLeftEdgeOfFrontFace = React.useCallback(() => [
-    // Left edge of front face from bottom to top
-    new THREE.Vector4(-2, -2, 0, 0.1),
-    new THREE.Vector4(-2, -1, 0, 0.1),
-    new THREE.Vector4(-2, 1, 0, 0.1),
-    new THREE.Vector4(-2, 2, 0, 0.1),
-  ], []);
+  const random = React.useCallback(
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+    (min = 0, max: number) => Math.random() * (max - min) + min,
+    [],
+  );
 
-  const getRightEdgeOfFrontFace = React.useCallback(() => [
-    // Right edge of front face from bottom to top
-    new THREE.Vector4(2, -2, 0, 0.1),
-    new THREE.Vector4(2, -1, 0, 0.1),
-    new THREE.Vector4(2, 1, 0, 0.1),
-    new THREE.Vector4(2, 2, 0, 0.1),
-  ], []);
+  const x0y0z0 = React.useMemo(() => [-2, -2, 0], []);
+
+  const x0y1z0 = React.useMemo(() => [-2, -1, 0], []);
+
+  const x0y2z0 = React.useMemo(() => [-2, 1, 0], []);
+
+  const x0y3z0 = React.useMemo(() => [-2, 2, 0], []);
+
+  const getLeftEdgeOfFrontFace = React.useMemo(
+    () => [
+      new THREE.Vector4(...x0y0z0, 0.1),
+      new THREE.Vector4(...x0y1z0, 0.1),
+      new THREE.Vector4(...x0y2z0, 0.1),
+      new THREE.Vector4(...x0y3z0, 0.1),
+    ],
+    [x0y0z0, x0y1z0, x0y2z0, x0y3z0],
+  );
+
+  const x1y0z0 = React.useMemo(() => [0, -2, 0], []);
+
+  const x1y1z0 = React.useMemo(() => [0, -1, 0], []);
+
+  const x1y2z0 = React.useMemo(() => [0, 1, 0], []);
+
+  const x1y3z0 = React.useMemo(() => [0, 2, 0], []);
+
+  const getMiddleEdgeOfFrontFace = React.useMemo(
+    () => [
+      new THREE.Vector4(...x1y0z0, 0.1),
+      new THREE.Vector4(...x1y1z0, 0.1),
+      new THREE.Vector4(...x1y2z0, 0.1),
+      new THREE.Vector4(...x1y3z0, 0.1),
+    ],
+    [x1y0z0, x1y1z0, x1y2z0, x1y3z0],
+  );
+
+  const x2y0z0 = React.useMemo(() => [2, -2, 0], []);
+
+  const x2y1z0 = React.useMemo(() => [2, -1, 0], []);
+
+  const x2y2z0 = React.useMemo(() => [2, 1, 0], []);
+
+  const x2y3z0 = React.useMemo(() => [2, 2, 0], []);
+
+  const getRightEdgeOfFrontFace = React.useMemo(
+    () => [
+      new THREE.Vector4(...x2y0z0, 0.1),
+      new THREE.Vector4(...x2y1z0, 0.1),
+      new THREE.Vector4(...x2y2z0, 0.1),
+      new THREE.Vector4(...x2y3z0, 0.1),
+    ],
+    [x2y0z0, x2y1z0, x2y2z0, x2y3z0],
+  );
+
+  const x2y0z3 = React.useMemo(() => [2, -2, -4], []);
+
+  const x2y1z3 = React.useMemo(() => [2, -1, -4], []);
+
+  const x2y2z3 = React.useMemo(() => [2, 1, -4], []);
+
+  const x2y3z3 = React.useMemo(() => [2, 2, -4], []);
+
+  const getRightEdgeOfRightFace = React.useMemo(
+    () => [
+      new THREE.Vector4(...x2y0z3, 0.1),
+      new THREE.Vector4(...x2y1z3, 0.1),
+      new THREE.Vector4(...x2y2z3, 0.1),
+      new THREE.Vector4(...x2y3z3, 0.1),
+    ],
+    [x2y0z3, x2y1z3, x2y2z3, x2y3z3],
+  );
+
+  const x0y0z3 = React.useMemo(() => [-2, -2, -4], []);
+
+  const x0y1z3 = React.useMemo(() => [-2, -1, -4], []);
+
+  const x0y2z3 = React.useMemo(() => [-2, 1, -4], []);
+
+  const x0y3z3 = React.useMemo(() => [-2, 2, -4], []);
+
+  const getLeftEdgeOfLeftFace = React.useMemo(
+    () => [
+      new THREE.Vector4(...x0y0z3, 0.1),
+      new THREE.Vector4(...x0y1z3, 0.1),
+      new THREE.Vector4(...x0y2z3, 0.1),
+      new THREE.Vector4(...x0y3z3, 0.1),
+    ],
+    [x0y0z3, x0y1z3, x0y2z3, x0y3z3],
+  );
 
   const net = React.useMemo(
     () => ({
@@ -30,14 +110,7 @@ const Cube: React.FC = () => {
        */
       back: {
         nsControlPoints: [
-          [
-            // Left edge of back face from bottom to top
-            // Same as right edge of right face
-            new THREE.Vector4(2, -2, -4, 0.1),
-            new THREE.Vector4(2, -1, -4, 0.1),
-            new THREE.Vector4(2, 1, -4, 0.1),
-            new THREE.Vector4(2, 2, -4, 0.1),
-          ],
+          getRightEdgeOfRightFace,
           [
             // Middle edge of back face from bottom to top
             new THREE.Vector4(0, -2, -4, 0.1),
@@ -87,15 +160,9 @@ const Cube: React.FC = () => {
        */
       front: {
         nsControlPoints: [
-          getLeftEdgeOfFrontFace(),
-          [
-            // Middle edge of front face from bottom to top
-            new THREE.Vector4(0, -2, 0, 0.1),
-            new THREE.Vector4(0, -1, 0, 0.1),
-            new THREE.Vector4(0, 1, 0, 0.1),
-            new THREE.Vector4(0, 2, 0, 0.1),
-          ],
-          getRightEdgeOfFrontFace(),
+          getLeftEdgeOfFrontFace,
+          getMiddleEdgeOfFrontFace,
+          getRightEdgeOfFrontFace,
         ],
         url: 'static/minecraft/dirt.jpg',
       },
@@ -104,13 +171,7 @@ const Cube: React.FC = () => {
        */
       left: {
         nsControlPoints: [
-          [
-            // Left edge of left face from bottom to top
-            new THREE.Vector4(-2, -2, -4, 0.1),
-            new THREE.Vector4(-2, -1, -4, 0.1),
-            new THREE.Vector4(-2, 1, -4, 0.1),
-            new THREE.Vector4(-2, 2, -4, 0.1),
-          ],
+          getLeftEdgeOfLeftFace,
           [
             // Middle edge of left face from bottom to top
             new THREE.Vector4(-2, -2, -2, 0.1),
@@ -118,7 +179,7 @@ const Cube: React.FC = () => {
             new THREE.Vector4(-2, 1, -2, 0.1),
             new THREE.Vector4(-2, 2, -2, 0.1),
           ],
-          getLeftEdgeOfFrontFace(),
+          getLeftEdgeOfFrontFace,
         ],
         url: 'static/minecraft/dirt.jpg',
       },
@@ -127,7 +188,7 @@ const Cube: React.FC = () => {
        */
       right: {
         nsControlPoints: [
-          getRightEdgeOfFrontFace(),
+          getRightEdgeOfFrontFace,
           [
             // Middle edge of right face from bottom to top
             new THREE.Vector4(2, -2, -2, 0.1),
@@ -135,13 +196,7 @@ const Cube: React.FC = () => {
             new THREE.Vector4(2, 1, -2, 0.1),
             new THREE.Vector4(2, 2, -2, 0.1),
           ],
-          [
-            // Right edge of right face from bottom to top
-            new THREE.Vector4(2, -2, -4, 0.1),
-            new THREE.Vector4(2, -1, -4, 0.1),
-            new THREE.Vector4(2, 1, -4, 0.1),
-            new THREE.Vector4(2, 2, -4, 0.1),
-          ],
+          getRightEdgeOfRightFace,
         ],
         url: 'static/minecraft/dirt.jpg',
       },
@@ -172,8 +227,15 @@ const Cube: React.FC = () => {
         url: 'static/minecraft/dirt.jpg',
       },
     }),
-    [],
+    [
+      getLeftEdgeOfFrontFace,
+      getMiddleEdgeOfFrontFace,
+      getRightEdgeOfFrontFace,
+      getRightEdgeOfRightFace,
+    ],
   );
+
+  console.log(net);
 
   return (
     <group>
@@ -181,8 +243,9 @@ const Cube: React.FC = () => {
       <Face {...{ ...net.right }} />
       <Face {...{ ...net.left }} />
       <Face {...{ ...net.back }} />
+      {/* <Face {...{ ...net.back }} />
       <Face {...{ ...net.top }} />
-      <Face {...{ ...net.bottom }} />
+      <Face {...{ ...net.bottom }} /> */}
     </group>
   );
 };
