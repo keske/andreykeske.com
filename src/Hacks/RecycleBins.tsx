63ffe9f0e1811f75409a922036e2d877d408914e
Andrey <hello@andreykeske.com>
2021-10-17 16:33:14 -0700

    Hacks/recycle bins (#101)
    * Init
    
    * Done

---
+++ src/Hacks/RecycleBins.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+
+// Libs
+import styled from 'styled-components';
+import { Col, Container, Row } from 'react-bootstrap';
+
+// Components
+import {
+  ProjectTopics,
+  ProjectWrapper,
+  ProjectYear,
+  Text,
+  Title,
+  UploadcareImage,
+} from '../components';
+
+const Details = styled.div`
+  opacity: 0.4;
+`;
+
+const StyledProjectWrapper = styled(ProjectWrapper)`
+  border-radius: 33px;
+  text-align: center;
+`;
+
+const StyledProjectYear = styled(ProjectYear)`
+  border: 1px solid #000;
+`;
+
+const StyledText = styled(Text)`
+  opacity: 0.5;
+`;
+
+const StyledTitle = styled(Title)`
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  background-image: linear-gradient(90deg, #e6344a, #3f497a);
+  background-repeat: repeat;
+  background-size: 100%;
+`;
+
+const Wrapper = styled.div`
+  padding: 33px;
+`;
+
+const RecycleBins: React.FC = (): JSX.Element => (
+  <Wrapper>
+    <StyledProjectWrapper>
+      <Container>
+        <Row>
+          <Col lg={{ offset: 0, span: 12 }}>
+            <UploadcareImage src="https://ucarecdn.com/a7680ff9-2c6b-4f5a-9fb8-13093b149933/" />
+
+            <StyledTitle>
+              Bins for a different types of files
+            </StyledTitle>
+            <StyledText>Separate your trash</StyledText>
+
+            <Details>
+              <ProjectTopics topics={['Inerface', 'Mac OS']} />
+              <StyledProjectYear>2021</StyledProjectYear>
+            </Details>
+          </Col>
+        </Row>
+      </Container>
+    </StyledProjectWrapper>
+  </Wrapper>
+);
+
+export default RecycleBins;
