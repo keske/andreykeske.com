3ce6b406c7e92df1f1e4e572d4996b34c009234d
Andrey <hello@andreykeske.com>
2021-10-17 20:50:10 -0700

    Linting

--- src/Hacks/Battleship.tsx
+++ src/Hacks/Battleship.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 // Libs
 import { StyleSheet, css } from 'aphrodite';
 import {
-  Col, Container, Row, Tab, Tabs,
+ Col, Container, Row, Tab, Tabs,
 } from 'react-bootstrap';
 
 // Components
@@ -26,23 +26,23 @@ const styles = StyleSheet.create({
 const Battleship: React.FC = (): JSX.Element => {
   const length = 10;
 
-  const letters: string[] = Array.from({ length }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
-
-  const numbers: number[] = Array.from(
-    { length },
-    (_, i) => i + 1,
+  const letters: string[] = Array.from({ length }, (_, i) =>
+    String.fromCharCode('A'.charCodeAt(0) + i),
   );
+
+  const numbers: number[] = Array.from({ length }, (_, i) => i + 1);
 
   return (
     <ProjectWrapper>
       <Container>
         <Row>
           <Col lg={{ offset: 3, span: 5 }}>
-
             <Tabs defaultActiveKey={letters[0]}>
               {letters.map((letter) => (
                 <Tab key={letter} eventKey={letter} title={letter}>
-                  <Tabs defaultActiveKey={numbers[numbers.length - 1]}>
+                  <Tabs
+                    defaultActiveKey={numbers[numbers.length - 1]}
+                  >
                     {numbers.map((number) => (
                       <Tab
                         key={`${letter}-${number}`}
