2d02208b340b07820446dde3c3db3df9f716887b
Andrey <hello@andreykeske.com>
2022-11-29 16:44:31 -0500

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+import { Element } from "@/components/index";
+
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>Andrey Keske</title>
+    </Head>
+    <Element />
+  </>
+);
+
+export default Home;
