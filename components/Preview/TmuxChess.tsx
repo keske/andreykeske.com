6a6824240207dc4685990cab6cda41e7af4180ca
Andrey <hello@andreykeske.com>
2022-11-29 21:57:34 -0500

    Added TmuxChess

--- components/Preview/TmuxChess.tsx
+++ components/Preview/TmuxChess.tsx
@@ -1,5 +1,47 @@
+import { useVideoTexture } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useMove } from "@use-gesture/react";
 import React from "react";
+import { Mesh } from "three";
 
-const TmuxChess = () => <>TmuxChess</>;
+const Video = () => {
+  const mesh = React.useRef<Mesh>(null!);
+
+  const texture = useVideoTexture("/tmux-chess.mp4", {
+    loop: true,
+    start: true,
+  });
+
+  useMove(
+    (state) => {
+      mesh.current.rotation.y += state.delta[0] * 0.001;
+      mesh.current.rotation.x += state.delta[1] * 0.001;
+    },
+    { target: document },
+  );
+
+  return (
+    <mesh ref={mesh} rotation={[-0.8, 0, 0]} scale={1}>
+      <planeGeometry args={[6, 6, 1]} />
+      <meshBasicMaterial map={texture} toneMapped={false} />
+    </mesh>
+  );
+};
+
+const TmuxChess = () => (
+  <div className="absolute top-0 left-0 h-screen w-screen bg-black">
+    <Canvas
+      gl={{
+        alpha: true,
+        antialias: true,
+        depth: false,
+        powerPreference: "high-performance",
+        stencil: false,
+      }}
+    >
+      <Video />
+    </Canvas>
+  </div>
+);
 
 export default TmuxChess;
