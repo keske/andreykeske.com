64a559891a77b7425fa607282a173cc79c75d257
Andrey <hello@andreykeske.com>
2022-12-01 20:46:21 -0500

    CloseButton styles

--- components/CloseButton.tsx
+++ components/CloseButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CloseButton: React.FC<Props> = ({ isShowing, onClick }) =>
   isShowing ? (
-    <div className="fixed top-5 right-5 cursor-pointer" onClick={onClick}>
+    <div className="fixed top-10 right-10 cursor-pointer" onClick={onClick}>
       Close
     </div>
   ) : null;
