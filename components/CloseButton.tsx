90fd762e7957e215500319ef0ebf4234993a370e
Andrey <hello@andreykeske.com>
2022-12-01 21:36:38 -0500

    Added escape handler to CloseButton

--- components/CloseButton.tsx
+++ components/CloseButton.tsx
@@ -5,11 +5,30 @@ type Props = {
   onClick: () => void;
 };
 
-const CloseButton: React.FC<Props> = ({ isShowing, onClick }) =>
-  isShowing ? (
+const CloseButton: React.FC<Props> = ({ isShowing, onClick }) => {
+  React.useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClick();
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClick]);
+
+  if (!isShowing) {
+    return null;
+  }
+
+  return (
     <div className="fixed top-10 right-10 cursor-pointer" onClick={onClick}>
       Close
     </div>
-  ) : null;
+  );
+};
 
 export default CloseButton;
