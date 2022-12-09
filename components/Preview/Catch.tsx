7df5d9ab4e42a62a194167e3fcf80ca3efb1a5ac
Andrey <hello@andreykeske.com>
2022-12-09 15:40:16 -0500

    Updated Catch

--- components/Preview/Catch.tsx
+++ components/Preview/Catch.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Catch = () => <>Catch</>;
+const Catch = () => (
+  <div className="absolute top-0 left-0 flex h-screen w-screen justify-center">
+    <iframe
+      allow="loop"
+      className="w-1/2 border-0 2xl:w-1/2"
+      src="/videos/catch.mp4"
+      title="The Catch"
+    />
+  </div>
+);
 
 export default Catch;
