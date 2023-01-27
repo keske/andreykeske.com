1685e397f6e69768bd08478ff7bfad942d015ea3
Andrey <hello@andreykeske.com>
2023-01-26 20:14:23 -0500

    Working with Graffity

--- components/Preview/Graffiti.tsx
+++ components/Preview/Graffiti.tsx
@@ -1,9 +1,16 @@
+import { animated, useSpring } from "@react-spring/web";
 import { useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useGesture } from "@use-gesture/react";
+
 import React from "react";
 import { Mesh } from "three";
 
 import { THREEOnMouseRotation } from "@/components/index";
+
+const random =
+  // eslint-disable-next-line @typescript-eslint/default-param-last
+  (min = 0, max: number) => Math.random() * (max - min) + min;
 
 const Model = () => {
   const mesh = React.useRef<Mesh>(null!);
@@ -11,7 +18,7 @@ const Model = () => {
   const { nodes } = useGLTF("/gltfs/wall.gltf") as GLTFResult;
 
   return (
-    <THREEOnMouseRotation ref={mesh}>
+    <THREEOnMouseRotation ref={mesh} strength={0.0001}>
       <mesh
         geometry={nodes.box_low.geometry}
         position={[0, 0, -40]}
@@ -24,42 +31,95 @@ const Model = () => {
   );
 };
 
-export const Graffiti = () => {
-  const peaces = [
-    "Keske, Kaze, 2007",
-    "Keske, Kaze, 2007",
-    "Keske, Kaze, 2007",
-    "Keske, Kaze, 2007",
-    "Kaze (Aru), Keske, 2007",
-    "Keske, Zebo (Bird), 2010",
-    "Keske, 2007",
-    "Keske (Skull and right thing), Kaze, 2007",
-    "Keske, Kaze (Aru), 2009",
-    "Keske, Keske, 2009",
-    "Ksenia, Keske, 2009",
-    "Keske, 2009",
-    "Keske, 2009",
-    "Kaze, Keske, 2007",
-    "Ksenia, Keske, 2009",
-  ];
+type PhotoProps = React.PropsWithChildren & {
+  index: number;
+};
+
+const Photo: React.FC<PhotoProps> = ({ children, index }) => {
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+
+  const bind = useGesture(
+    {
+      onDrag: ({ offset: [ox, oy] }) => {
+        api.start({
+          immediate: true,
+          x: ox,
+          y: oy,
+        });
+      },
+    },
+    { drag: { preventDefault: true } },
+  );
 
   return (
-    <>
-      <div className="flex flex-col items-center gap-10 py-40">
-        {peaces.map((peace, index) => (
-          <div className="md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/3" key={index}>
+    <animated.div
+      {...bind()}
+      className="absolute flex w-1/4 cursor-grab flex-col gap-1 bg-slate-300 p-1"
+      style={{
+        left: random(0, window.innerWidth),
+        top: random(0, window.innerHeight),
+        x,
+        y,
+        zIndex: index,
+      }}
+    >
+      {children}
+    </animated.div>
+  );
+};
+
+export const Graffiti = () => {
+  const peaces = React.useMemo(
+    () => [
+      "Keske, Kaze, 2007",
+      "Keske, Kaze, 2007",
+      "Keske, Kaze, 2007",
+      "Keske, Kaze, 2007",
+      "Kaze (Aru), Keske, 2007",
+      "Keske, Zebo (Bird), 2010",
+      "Keske, 2007",
+      "Keske (Skull and right thing), Kaze, 2007",
+      "Keske, Kaze (Aru), 2009",
+      "Keske, Keske, 2009",
+      "Ksenia, Keske, 2009",
+      "Keske, 2009",
+      "Keske, 2009",
+      "Kaze, Keske, 2007",
+      "Ksenia, Keske, 2009",
+    ],
+    [],
+  );
+
+  const renderPeaces = React.useMemo(
+    () =>
+      peaces.map((peace, index) => (
+        <div
+          key={index}
+          style={{
+            left: random(0, window.innerWidth),
+            top: random(0, window.innerHeight),
+            zIndex: index,
+          }}
+        >
+          <Photo index={index}>
             <img
               alt={peace}
               className="w-full"
               src={`/photos/graffiti/${index}.jpg`}
             />
-            <p>
+            <p className="text-black">
               {peace}, <span className="opacity-30">Yekaterinburg, Russia</span>
             </p>
-          </div>
-        ))}
-      </div>
-      <div className="fixed top-0 z-10 h-[100px] w-[100px]">
+          </Photo>
+        </div>
+      )),
+    [peaces],
+  );
+
+  return (
+    <>
+      <div className="h-screen w-screen bg-slate-800">{renderPeaces}</div>
+      <div className="fixed top-0 z-0 h-screen w-screen">
         <Canvas
           gl={{
             alpha: true,
