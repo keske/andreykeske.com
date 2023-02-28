f31eed74e5bb98a5caf7779a7c67ea8d200c6a85
Andrey <hello@andreykeske.com>
2023-02-28 18:35:34 -0500

    Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,52 +1,70 @@
 import clsx from "clsx";
 import React from "react";
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type Props = {
   as?: React.ElementType;
   size?: "lg" | "md" | "sm" | "xl" | "xs" | null;
   variant?: "primary" | "secondary" | "transparent";
 };
 
-export type ButtonRef =
-  | HTMLButtonElement
-  | HTMLDivElement
-  | HTMLLinkElement
-  | HTMLSpanElement;
+type AsHTMLButtonElement = Props &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = React.forwardRef<ButtonRef, ButtonProps>(
-  (
+type AsHTMLAnchorElement = Pick<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  "disabled"
+> &
+  Props &
+  React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+export type ButtonProps<T> = T extends HTMLButtonElement
+  ? AsHTMLButtonElement
+  : AsHTMLAnchorElement;
+
+export type ButtonRef<T extends HTMLElement> = React.Ref<T>;
+
+export const Button = React.forwardRef(function <T extends HTMLElement>(
+  {
+    as = "button",
+    children,
+    className,
+    disabled = false,
+    size = "md",
+    type = "button",
+    variant = "primary",
+    ...props
+  }: ButtonProps<T>,
+  ref: ButtonRef<T>,
+) {
+  return React.createElement(
+    as,
     {
-      as = "button",
-      children,
-      className,
-      disabled = false,
-      size = "md",
-      type = "button",
-      variant = "primary",
-      ...props
-    },
-    ref,
-  ) =>
-    React.createElement(
-      as,
-      {
-        className: clsx(className, "cursor-pointer rounded-full border-none", {
-          // variant states
+      ...(as === "button" && { disabled }),
+      className: clsx(
+        className,
+        "cursor-pointer rounded-full border-none",
+        {
+          // `variant` states
           "bg-black text-white": variant == "primary",
           "bg-transparent": variant == "transparent",
           "bg-white": variant == "secondary",
-
-          // size states
+        },
+        {
+          // `size` states
           "p-5 text-lg": size == "lg",
-          "py-2 text-xs px-3": size == "xs",
+          "py-1 text-xs": size == "xs",
           "py-3 px-5 text-sm": size == "sm",
           "text-md px-5 py-4": size == "md",
-        }),
-        disabled,
-        ref,
-        type,
-        ...props,
-      },
-      children,
-    ),
-);
+        },
+        {
+          // `anchor` styles
+          "pointer-events-none": as === "a" && disabled,
+        },
+      ),
+      ref,
+      type,
+      ...props,
+    },
+    children,
+  );
+});
