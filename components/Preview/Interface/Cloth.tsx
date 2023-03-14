59d36daf7d6f7ace1f1bd102b9024503f0fcb0e7
Andrey <hello@andreykeske.com>
2023-03-14 19:10:16 -0400

    Fixes: removed localhost from iframes

--- components/Preview/Interface/Cloth.tsx
+++ components/Preview/Interface/Cloth.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Cloth = () => (
   <iframe
     className="h-screen w-screen border-0"
-    src="http://localhost:3000/examples/cloth/terminal.html"
+    src="/examples/cloth/terminal.html"
     title="Soft Video Player"
   />
 );
--- components/Preview/Interface/SoftVideoPlayer.tsx
+++ components/Preview/Interface/SoftVideoPlayer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SoftVideoPlayer = () => (
   <iframe
     className="h-screen w-screen border-0"
-    src="http://localhost:3000/examples/soft-ui/examples/soft-player/index.html"
+    src="/examples/soft-ui/examples/soft-player/index.html"
     title="Soft Video Player"
   />
 );
