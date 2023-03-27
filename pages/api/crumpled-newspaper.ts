3a2b00d1aa5e100f473c1d1b42c3e9846e8f95e7
Andrey <hello@andreykeske.com>
2023-03-27 17:56:17 -0400

    Trying to opimize screen shoting

--- pages/api/crumpled-newspaper.ts
+++ pages/api/crumpled-newspaper.ts
@@ -16,14 +16,51 @@ export default async function handler(
 
   const getScreenshot = async () => {
     const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
+      args: [
+        ...chromium.args,
+        "--autoplay-policy=user-gesture-required",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-client-side-phishing-detection",
+        "--disable-component-update",
+        "--disable-default-apps",
+        "--disable-dev-shm-usage",
+        "--disable-domain-reliability",
+        "--disable-extensions",
+        "--disable-features=AudioServiceOutOfProcess",
+        "--disable-hang-monitor",
+        "--disable-ipc-flooding-protection",
+        "--disable-notifications",
+        "--disable-offer-store-unmasked-wallet-cards",
+        "--disable-popup-blocking",
+        "--disable-print-preview",
+        "--disable-prompt-on-repost",
+        "--disable-renderer-backgrounding",
+        "--disable-setuid-sandbox",
+        "--disable-speech-api",
+        "--disable-sync",
+        "--hide-scrollbars",
+        "--ignore-gpu-blacklist",
+        "--metrics-recording-only",
+        "--mute-audio",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--no-pings",
+        "--no-sandbox",
+        "--no-zygote",
+        "--password-store=basic",
+        "--use-gl=swiftshader",
+        "--use-mock-keychain",
+      ],
       executablePath,
-      headless: false,
+      headless: true,
     });
 
     const page = await browser.newPage();
 
-    await page.goto(req.body.url);
+    await page.goto(req.body.url, { waitUntil: "networkidle2" });
 
     await page.setViewport({ height: 768, width: 768 });
 
