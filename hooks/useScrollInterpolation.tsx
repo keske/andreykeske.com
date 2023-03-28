c7bb71c20c296eeecb9b92a20e76f80d6828ae00
Andrey <hello@andreykeske.com>
2023-03-28 18:37:16 -0400

    Edited: CrumpledNewspaper, Ticky and BTCBubble

---
+++ hooks/useScrollInterpolation.tsx
@@ -0,0 +1,36 @@
+import { useGesture } from "@use-gesture/react";
+import React from "react";
+
+import { interpolate } from "@/utils/index";
+
+export const useScrollInterpolation = (
+  from: number,
+  to: number,
+  initialValue: number,
+) => {
+  const [opacity, setOpacity] = React.useState(initialValue);
+
+  useGesture(
+    {
+      onScroll: () => {
+        const { clientHeight, scrollHeight, scrollTop } =
+          document.documentElement;
+
+        const opacityValue = interpolate(
+          [0, scrollHeight - clientHeight],
+          [from, to],
+          scrollTop,
+        );
+
+        setOpacity(opacityValue);
+      },
+    },
+    { target: window },
+  );
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
+  return opacity;
+};
