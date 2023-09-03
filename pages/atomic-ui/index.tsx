2f8a0213a99de712c60d40a9d21dcb08de2c4a5d
Andrey <hello@andreykeske.com>
2023-09-03 14:04:35 -0400

    Some progress

--- pages/atomic-ui/index.tsx
+++ pages/atomic-ui/index.tsx
@@ -1,23 +1,30 @@
-import { Box } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { Canvas } from "@react-three/fiber";
+import {
+  Box,
+  OrbitControls,
+  RoundedBox,
+  Sphere,
+  Text,
+  Text3D,
+} from "@react-three/drei";
+import {
+  Canvas,
+  MeshProps,
+  ReactThreeFiber,
+  useFrame,
+} from "@react-three/fiber";
 import React from "react";
 import * as THREE from "three";
 
 import type { NextPage } from "next";
 
-///////////////////////////////////////////////////////////////////
-
-const AnimatedBox = () => {
-  const density = 0.0000005;
-
+const useNoiseMaterial = (density: number = 0.0000005) => {
   const material = new THREE.ShaderMaterial({
     fragmentShader: `
       uniform float time;
       uniform float density;
       varying vec2 vUv;
       float random(vec2 st) {
-        return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453);
+        return fract(sin(dot(st.xy, vec2(112.9898, 178.233))) * 43758.5453);
       }
       void main() {
         float noiseValue = random(vUv * density) * 2.0 - 1.0;
@@ -37,98 +44,100 @@ const AnimatedBox = () => {
     `,
   });
 
-  useFrame(({ clock }) => {
+  useFrame(({ clock }, delta) => {
     material.uniforms.time.value = clock.getElapsedTime();
 
-    material.uniforms.density.value = Math.asin(
-      clock.getElapsedTime() * 0.00001,
+    material.uniforms.density.value = Math.sin(
+      clock.getElapsedTime() * 0.000001,
     );
+    // material.uniforms.density.value += delta / 100000;
+
+    // console.log(Math.sin(clock.getElapsedTime() * 0.000001));
   });
+
+  return material;
+};
+
+type NoiseBoxProps = Partial<
+  Omit<MeshProps, "args"> & {
+    args?:
+      | [
+          width?: number | undefined,
+          height?: number | undefined,
+          depth?: number | undefined,
+          widthSegments?: number | undefined,
+          heightSegments?: number | undefined,
+          depthSegments?: number | undefined,
+        ]
+      | undefined;
+  }
+>;
+
+const NoiseBox = React.forwardRef<THREE.Mesh, NoiseBoxProps>(
+  (props, forwardedRef) => {
+    const material = useNoiseMaterial(0.000000005);
+
+    return <Box material={material} ref={forwardedRef} {...props} />;
+  },
+);
+
+export declare type NamedArrayTuple<T extends (...args: any) => any> =
+  Parameters<T>;
+
+type NoiseSphereRoundedBox = Omit<MeshProps, "args"> & {
+  args?: NamedArrayTuple<
+    (width?: number, height?: number, depth?: number) => void
+  >;
+  creaseAngle?: number;
+  radius?: number;
+  smoothness?: number;
+  steps?: number;
+};
+
+const NoiseRoundedBox = React.forwardRef<
+  THREE.Mesh,
+  NoiseSphereRoundedBox
+>((props, forwardedRef) => {
+  const material = useNoiseMaterial();
 
   return (
-    <Box args={[1, 1, 1]} material={material}>
-      <mesh />
-    </Box>
+    <RoundedBox material={material} ref={forwardedRef} {...props} />
   );
-};
+});
 
-///////////////////////////////////////////////////////////////////
+type NoiseSphereProps = Partial<
+  Omit<MeshProps, "args"> & {
+    args?:
+      | [
+          radius?: number | undefined,
+          length?: number | undefined,
+          capSegments?: number | undefined,
+          radialSegments?: number | undefined,
+        ]
+      | undefined;
+  }
+>;
 
-export type AtomicSurfaceProps = {
-  color?: THREE.Color | number | string;
-  density?: number;
-  height?: number;
-  width?: number;
-};
+const NoiseSphere = React.forwardRef<THREE.Mesh, NoiseSphereProps>(
+  (props, forwardedRef) => {
+    const material = useNoiseMaterial();
 
-export const AtomicSurface: React.FC<AtomicSurfaceProps> = ({
-  color = "#FFF",
-  density = 10000,
-  height = 1,
-  width = 2,
-}) => {
-  const ref = React.useRef<
-    HTMLDivElement & {
-      geometry: THREE.BoxGeometry;
-    }
-  >();
-
-  const particlePositions = React.useRef(
-    new Float32Array(density * 3),
-  );
-
-  const setPosition = React.useCallback(() => {
-    const particles = new THREE.BufferGeometry();
-
-    [...Array(density)].forEach((_, i) => {
-      const x = Math.random() * width - width / 2;
-
-      const y = Math.random() * height - height / 2;
-
-      const z = (Math.random() * height) / 2;
-
-      particlePositions.current[i * 3] = x;
-      particlePositions.current[i * 3 + 1] = y;
-      particlePositions.current[i * 3 + 2] = z;
-    });
-
-    return new THREE.Points(particles);
-  }, [density, height, width]);
-
-  const cloud = React.useMemo(() => {
-    const material = new THREE.PointsMaterial({
-      blending: THREE.AdditiveBlending,
-      color,
-      sizeAttenuation: false,
-    });
-
-    const particles = new THREE.BufferGeometry();
-
-    particles.setDrawRange(0, density);
-
-    particles.setAttribute(
-      "position",
-      new THREE.BufferAttribute(
-        particlePositions.current,
-        3,
-      ).setUsage(THREE.DynamicDrawUsage),
+    return (
+      <Sphere material={material} ref={forwardedRef} {...props} />
     );
+  },
+);
 
-    return new THREE.Points(particles, material);
-  }, [color, density]);
+type NoiseTextProps = React.PropsWithChildren;
 
-  useFrame(() => {
-    setPosition();
+const NoiseText = React.forwardRef<THREE.Mesh, NoiseTextProps>(
+  (props, forwardedRef) => {
+    const material = useNoiseMaterial(0.0000005);
 
-    if (ref.current) {
-      ref.current.geometry.attributes.position.needsUpdate = false;
-    }
-  });
-
-  return <primitive object={cloud} ref={ref} />;
-};
-
-///////////////////////////////////////////////////////////////////
+    // @ts-expect-error WIP
+    return <Text material={material} ref={forwardedRef} {...props} />;
+  },
+);
 
 const AtomicUI: NextPage = () => (
   <div className="h-screen w-screen">
@@ -138,8 +147,15 @@ const AtomicUI: NextPage = () => (
       }}
       className="h-screen w-screen"
     >
-      {/* <AtomicSurface /> */}
-      <AnimatedBox />
+      <OrbitControls />
+      <NoiseBox args={[1, 1, 1]} position={[1, 2, 1]} />
+      <NoiseSphere position={[1, -2, 1]} />
+      <NoiseRoundedBox
+        args={[1, 0.3, 0.1]}
+        position={[-2, -2, 1]}
+        radius={0.09}
+      />
+      <NoiseText position={[1, 2, 2]}>METHAPHORS</NoiseText>
     </Canvas>
   </div>
 );
