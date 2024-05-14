0c2f3938418cdf5bd2d5b92e13d406e0a2e5770d
Andrey <hello@andreykeske.com>
2024-05-14 13:31:32 -0400

    Some ui button updates

--- pages/shader-ui/index.tsx
+++ pages/shader-ui/index.tsx
@@ -7,9 +7,14 @@ import { Button } from "@/packages/shader-ui";
 const AtomicUI: NextPage = () => (
   <div className="w-scree h-screen bg-slate-900 p-10">
     <div className="flex flex-col gap-8">
-      <Button size="xs">Button</Button>
+      <Button shader="noise" size="xs">
+        Button
+      </Button>
       <Button size="sm">Button</Button>
       <Button size="md">Button</Button>
+      <Button shader="starfield" size="md">
+        Button
+      </Button>
       <Button size="lg">Button</Button>
 
       {/* <Button shader="starfield" size="xs">
